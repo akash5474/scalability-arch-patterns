c4b2ae77f19d541297ed92eafc9c01dc07663feb
Akash Pannu <akash5474@gmail.com>
2015-05-14 12:38:29 -0400

    Added logging of PID to cluster-master

--- vertical-scaling/clusteredServer.js
+++ vertical-scaling/clusteredServer.js
@@ -2,9 +2,12 @@ import cluster from 'cluster';
 import os from 'os';
 import server from './server';
 
+const pid = process.pid;
+
 if ( cluster.isMaster ) {
   let forkableCPUs = os.cpus().length - 1;
-  console.log('cpus', forkableCPUs);
+  console.log('PID:', pid);
+  console.log('forkableCPUs:', forkableCPUs);
   for ( let i = 0; i < forkableCPUs; i++ ) {
     cluster.fork();
   }
